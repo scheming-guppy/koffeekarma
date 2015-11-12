358133c80c053982daeae7f533e381c9654a2b14
William Henshaw <whenshaw87@googlemail.com>
2015-11-12 11:12:14 -0800

    Made model changes

--- server/models/index.js
+++ server/models/index.js
@@ -11,11 +11,26 @@ module.exports = {
         callback(err, results);
       });
     },
-    signup : function (params, callback) {
+    signup: function (params, callback) {
       var queryStr = "INSERT INTO users (userName, firstName, lastName, password, ticketSent, ticketAvailable) VALUES (" + params[0] + ", " + params[1] + ", " + params[2] + ", " + params[3] + ", 1, 0);";
       db.query(queryStr, function(err, results) {
         callback(err, results);
       });
+    },
+    send: function (params, callback) {
+      var randomId = "SELECT id FROM users ORDER BY RAND() LIMIT 1;";
+      var createTicket = "INSERT INTO tickets (redeemed, sentBy, receivedBy, message) "
+      //query userdatabase for random user
+      //once id - insert ticketdataase
+      //make sure user that ticket is being sent to is not the same as the one being from
+      db.query(queryStr, function(err, results) {
+        callback(err, results);
+      });
+    },
+    redeem: function (params, callback) {
+      var queryStr = "";
+      db.query(queryStr, function(err, results) {
+        callback(err, results);
+      })
     }
-
   }
