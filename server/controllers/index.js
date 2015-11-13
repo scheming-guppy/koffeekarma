480f5436d2389b38e6007bacd0bb904a541eaa0e
William Henshaw <whenshaw87@googlemail.com>
2015-11-13 15:46:18 -0800

    Got backend almost working

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,37 +4,71 @@ module.exports = {
 
   signin: {
     post: function (req, res) {
+      console.log("In post woooooo");
       var params = [req.body.userName, req.body.password];
       models.users.signin(params, function(err, results) {
-        if (err) { /* do something */ }
-        res.sendStatus(201);
+        if (err) { 
+          res.sendStatus(401)
+        } else {
+          if ( results.password === req.body.password ) {
+            console.log( "successfuly signin after pw check" );
+          }
+          console.log("results from controller signin", results);
+          res.send(results);
+
+        }
+        // res.sendStatus(201);
+        // need to hash req.body.password here and then compare to the
+        // results password
       });
     }
   },
   signup: {
     post: function (req, res) {
+      console.log("In the controller signup function........", req.body)
      var params = [req.body.userName, req.body.firstName, req.body.lastName, req.body.password]//image??
      models.users.signup(params, function (err, results){
-      if (err) { }
-        res.sendStatus(201);
+      console.log("This is results.....", results)
+      // if ( results === false ) { err = true; }
+      if (err) { 
+        console.log("Signup failed!", err); 
+        res.sendStatus(401) 
+      } else {
+
+      console.log("Is this wroking headers")
+      // res.sendStatus(201);
+      res.send(results)
+      } 
+
      });
     }
   },
   send: {
     post: function (req, res) {
+      var params = [req.body.id, req.body.message];
+      console.log("params in controllers", params)
+      models.users.send(params, function (err, results){
+        if (err) {
+          res.sendStatus(401);
+        } else {
+          res.sendStatus(201);
+        }
+      });
+    }
+  },
+  redeem: {
+    post: function (req, res) {
+      console.log("This is req.body.id...", req.body.id)
       var params = [req.body.id];
-        models.tickets.send(params, function (err, results){
-      if (err) { }
-        res.sendStatus(201);
-     });
-    },
-    redeem: {
-      post: function (req, res) {
-        var params = [req.body.id];
-        models.tickets.redeem(params, function (err, results){
-            if (err) { }
-              res.sendStatus(201)
-           });
-         }
-      }
-  };
+      console.log('params in controllers', params)
+      models.users.redeem(params, function (err, results) {
+        if (err) {
+          res.sendStatus(401);
+        } else {
+          res.sendStatus(201);
+        }
+          // res.sendStatus(201);
+      });
+    }
+  }
+}
